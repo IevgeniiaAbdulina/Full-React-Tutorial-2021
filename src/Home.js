3626fd34de4fcf67f6dbfe2268f6e3e35008823e
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-01-28 14:31:27 +0100

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+    const handleClick = (event) => {
+        console.log("hello, ninjas", event)
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("hello " + name)
+    }
+
+    const handleClickAgainTwo = (name, event) => {
+        console.log("hello " + name, event.target)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
+            <button onClick={(event) => handleClickAgainTwo('Mario', event)}>Click me again and again</button>
+
         </div>
     );
 }
