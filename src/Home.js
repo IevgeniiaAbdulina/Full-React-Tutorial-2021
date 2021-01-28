7c645e9e414f9d882499c6503b32e0652225b5f8
IevgeniiaAbdulina <ievgeniiaabdulina@gmail.com>
2021-01-28 15:24:54 +0100

    Using State(useState hook)

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
-    const handleClick = (event) => {
-        console.log("hello, ninjas", event)
-    }
 
-    const handleClickAgain = (name) => {
-        console.log("hello " + name)
-    }
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState('25');
 
-    const handleClickAgainTwo = (name, event) => {
-        console.log("hello " + name, event.target)
+    const handleClick = () => {
+        setName('Luigi');
+        setAge('30');
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
-            <button onClick={(event) => handleClickAgainTwo('Mario', event)}>Click me again and again</button>
-
         </div>
     );
 }
